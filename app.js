45ad616d3fc998e7686a083d039da7be109cce61
USER2 <USER2>
2017-09-28 23:51:54 +0900

    Add files

---
+++ app.js
@@ -0,0 +1,16 @@
+(function () {
+'use strict';
+
+angular.module('VibrationApp',['angular-vibrator'])
+      .controller('AppController', AppController);
+
+      AppController.$inject=['$scope','vibrator'];
+      function AppController($scope, vibrator){
+        var $ctrl=this;
+        $ctrl.duration=10000;
+        $ctrl.vibrate=function(duration){
+          console.log('start vibration');
+          vibrator.vibrate(duration);
+        };
+      };
+})();
